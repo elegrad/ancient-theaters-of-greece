811e2dd182c114a756ca31b345ef677b8d6ca206
Christos Chiotis <support@elegrad.com>
2021-02-05 15:28:49 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 var config = {
-  geojson: "https://gist.githubusercontent.com/chiotis/0ac1c21db3429287c6f3942c5eeffb86/raw/0cd29abd115cf42a256fcaeff6e175663f75553b/map.geojson",
+  geojson: "https://gist.githubusercontent.com/JJediny/b238fc15e3b31a2e3432/raw/3d73dbd7d4c950c8e85fdb7485567e84eca90772/CitizenScienceandCrowdsourcing_WWilson.geojson",
   title: "Διάζωμα Αρχαία Θέατρα",
   layerName: "Projects",
   hoverProperty: "project_name",
